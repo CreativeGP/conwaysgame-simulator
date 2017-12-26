1b7bfbbc55aee07487f547b88103ae6d547ab8d0
CreativeGP <malicktafaminecraft@gmail.com>
2017-12-26 17:22:57 +0900

    [add] RLEデータ操作関数

--- rle.js
+++ rle.js
@@ -4,7 +4,10 @@ function get_rle_info(data) {
     {
 	let newlines = [];
 	for (let i = 0, l; l = lines[i]; ++i) {
-	    if (l[0] == '#') ;
+	    if (l[0] == '#'){
+		let kind_of_comment = l[1];
+		if (kind_of_comment == '');
+	    }
 	    else newlines.push(l);
 	}
 	lines = newlines;
@@ -25,7 +28,54 @@ function get_rle_info(data) {
     return keywords;
 }
 
+function get_raw_rle(data) {
+    let lines = data.split('\n');
+    // コメントを飛ばす
+    {
+	let newlines = [];
+	for (let i = 0, l; l = lines[i]; ++i) {
+	    if (l[0] == '#') ;
+	    else newlines.push(l);
+	}
+	lines = newlines;
+    }
+
+    return lines.splice(1).join();
+}
 
 function get_rle_dimension(data) {
-    
+    let info = get_rle_info(data);
+    let result = { x: info['x'], y: info['y'] };
+    {
+	let maxx=0, maxy=0;
+	let rawdata = get_raw_rle(data);
+	let keywords = get_rle_info(data);
+	let numeric = '',
+	    xcursor = 0,
+	    ycursor = 0;
+	for (let i = 0, d; d = rawdata[i]; i++) {
+	    if ($.isNumeric(d)) {
+		numeric += d;
+		continue;
+	    }
+	    let num = numeric == '' ? 1 : Number(numeric);
+	    if (d == 'o' || d == 'b') {
+		xcursor += num;
+	    }
+	    if (d == '$') {
+		for(let j=0;j<num;j++) {
+		    xcursor = 0;
+		    ycursor++;
+		    maxy = Math.max(maxy, ycursor);
+		}
+	    }
+	    maxx = Math.max(maxx, xcursor);
+	    numeric = '';
+	}
+
+	result.width = maxx;
+	result.height = maxy + 1;
+    }
+
+    return result;
 }
